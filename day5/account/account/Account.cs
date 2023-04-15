0c0d11d1c9005cef9a55d51a8e3a631887b207eb
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-15 19:09:53 +0530

    Add files via upload

---
+++ day5/account/account/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AccountDemoApp
+{
+    class Account
+    {
+        public int Id;
+        public string Name;
+        public decimal Balance;
+
+        public void CreateAccount(int id, string name, decimal balance)
+        {
+            Id = id;
+            Name = name;
+            Balance = balance;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Account Id :" + Id);
+            Console.WriteLine("Name :" + Name);
+            Console.WriteLine("Balance :" + Balance);
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        public void withdraw(decimal amount)
+        {
+            if(Balance<amount)
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+            else
+            {
+                Balance -= amount;
+                Console.WriteLine("Availble balance is:" + Balance);
+            }
+            
+        }
+
+
+
+
+        
+    }
+}
+    
---
+++ day5/sorting/sorting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[] intArray = new int[5];
+            Console.WriteLine("Enter the Array Elements : ");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+            //Sorting the array
+            for (int j = 0; j <= intArray.Length - 2; j++)
+            {
+                //intArray.Length - 2
+                for (int i = 0; i <= intArray.Length - 2; i++)
+                {
+                    count = count + 1;
+                    if (intArray[i] > intArray[i + 1])
+                    {
+                        int temp = intArray[i + 1];
+                        intArray[i + 1] = intArray[i];
+                        intArray[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After Sorting Array :");
+            foreach (int item in intArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Loop iterates :" + count);
+            Console.ReadKey();
+        }
+    }
+}
