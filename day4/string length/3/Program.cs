4ba7a3f0c00a8a2f77e1c91fdfee401a70a1d727
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-15 19:08:12 +0530

    Add files via upload

---
+++ day4/string length/3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Write a program in C# Sharp to find the length of a string without using library function. Go to the editor
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int l= 0;
+            Console.WriteLine("enter the string");
+                a = Console.ReadLine();
+            foreach(char ch in a)
+            {
+                l= l+1;
+
+            }
+            Console.WriteLine("length is :" + l);
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ day4/string/1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("ENTER THE STRING");
+            str = Console.ReadLine();
+            Console.WriteLine("The string you are entered " +str);
+            Console.ReadKey();
+        }
+    }
+}
