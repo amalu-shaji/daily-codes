c6c10b4f1386e00385a1d074db327a2a909cbf37
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-15 19:41:52 +0530

    Add files via upload

---
+++ day6/account/account/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AccountDemoApp
+{
+    class Account
+    {
+        public int Id;
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length > 15)
+                {
+                    throw new ApplicationException("Name is too big");
+                    _name = value;
+                }
+            }
+        }
+
+        private decimal _balance;
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+        public void CreateAccount(int id, string name, decimal balance)
+        {
+            Id = id;
+            Name = name;
+            _balance = balance;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Account Id :" + Id);
+            Console.WriteLine("Name :" + Name);
+            Console.WriteLine("Balance :" + Balance);
+        }
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+        public void withdraw(decimal amount)
+        {
+            if(_balance<amount)
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+            else
+            {
+                _balance -= amount;
+                
+            }
+            
+        }
+
+
+
+
+        
+    }
+}
+    
