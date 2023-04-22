5d5c22467030b44a50925d7c42838b4a6223883e
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-22 11:10:04 +0530

    Add files via upload

---
+++ day10/namespace/namespace/Program.cs
@@ -0,0 +1,35 @@
+﻿using @namespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @namespace
+{
+    public class array
+    {
+        float[] arr = new float[] { 12.3f, 34.9f, 3f };
+        public float this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+    }
+    class class1
+    {
+        static void Main(string[] args)
+        {
+            array a;
+            a = new array();
+            a[3] = 34.9f;
+            Console.WriteLine(a[3]);
+        }
+    }
+}
