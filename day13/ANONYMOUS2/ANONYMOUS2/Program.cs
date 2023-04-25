731ce7b5c2548dfd901c6b5a084a3a36fed25271
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-25 16:00:58 +0530

    Add files via upload

---
+++ day13/ANONYMOUS2/ANONYMOUS2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANONYMOUS2
+{
+
+    internal class Program
+    {
+        public delegate int Sum(int x, int y);
+        static void Main(string[] args)
+        {
+            int result;
+            {
+                Sum s = delegate (int m, int n)
+                {
+                    return n + m;
+                };
+                result = s(10, 20);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
