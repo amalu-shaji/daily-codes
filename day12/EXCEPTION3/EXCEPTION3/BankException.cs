608422c4e9d321bcace04c68663b6939dc4d1761
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-25 10:02:18 +0530

    Add files via upload

---
+++ day12/EXCEPTION3/EXCEPTION3/BankException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXCEPTION3
+{
+    class BankException : Exception
+
+    {
+
+        int acc;
+
+        int bal;
+
+        public BankException(int a, int b)
+
+        {
+
+            acc = a;
+
+            bal = b;
+
+        }
+
+        public void inform()
+
+        {
+
+            Console.WriteLine("Account Number: " + acc
+
+         + " Balance left: " + bal);
+
+        }
+
+    }
+}
+
---
+++ day12/exception/exception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 1;
+            int i=0;
+
+        try
+            {
+                k = k / i;
+                System.Console.WriteLine("HELLO");
+               
+            }
+
+        catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
