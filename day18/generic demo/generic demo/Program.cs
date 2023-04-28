132f01afacc0e1b1d9499251d6f9bfdb9b8d079b
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-28 14:44:25 +0530

    Add files via upload

---
+++ day18/generic demo/generic demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList s = new SortedList();
+            s.Add("maharashtra  ", "mumbai");
+            s.Add("karnataka    ", "bangalore");
+            s.Add("andrapradesh  ", "hyderabad");
+            s.Add("tamilnadu     ", "chennai");
+            s.Add("bihar         ", "patna");
+            s.Add("rajasthan      ", "jaipur");
+            Console.WriteLine("elements in the sorted list:");
+            foreach (var item in s.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            foreach (var item in s.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ day18/list/list/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            names.Add("amalu");
+            names.Add("anu");
+            names.Add("sam");
+            names.Add("ram");
+            names.Add("kaethik");
+            names.Add("manju");
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
