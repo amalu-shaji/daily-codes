0b2e5eaafc6ce090b4ebe0b856ee1e5c1def0732
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-04-28 14:21:26 +0530

    Add files via upload

---
+++ day11/CarPoolingSystem_Requirement/CarPoolingSystemRequirement5/member5.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+uusing System;
+
+
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Requirement_5
+
+{
+
+    public class Member
+
+    {
+
+        string firstname;
+
+        string lastname;
+
+        string emailid;
+
+        public Member()
+
+        {
+
+        }
+
+        public Member(string firstname, string lastname, string emailid)
+
+        {
+
+            this.firstname = firstname;
+
+            this.lastname = lastname;
+
+            this.emailid = emailid;
+
+        }
+
+        public override string ToString()
+
+        {
+
+            return string.Format($"Member:{firstname},{lastname}{emailid}");
+
+        }
+
+        public void checking(string email)
+
+        {
+
+            if (email.Contains("@") && ((email.EndsWith(".com")) || (email.EndsWith(".org"))))
+
+            {
+
+                Console.WriteLine("valid mail");
+
+            }
+
+            else
+
+            {
+
+                throw new InvalidEmailException(email);
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
---
+++ day11/CarPoolingSystem_Requirement/CarPoolingSystem_Req 2/MemberCars.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPoolingSystem_Requirement
+{
+	class MemberCars
+	{
+		private long _id;
+
+		public long Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+		private Member _member;
+
+		public Member Member
+		{
+			get { return _member; }
+			set { _member = value; }
+		}
+		private Car _car;
+
+		public Car Car
+		{
+			get { return _car; }
+			set { _car = value; }
+		}
+		private string _carRegistrationNumber;
+
+		public string CarRegistrationNumber
+		{
+			get { return _carRegistrationNumber; }
+			set { _carRegistrationNumber = value; }
+		}
+
+
+		private string _carColor;
+
+		public string CarColor
+		{
+			get { return _carColor; }
+			set { _carColor = value; }
+		}
+		public MemberCars()
+		{
+		}
+		public MemberCars(long _id, Member _member, Car _car, string _carRegistrationNumber, string _carColor)
+	{	
+		    Id = _id;
+		    Member = _member;
+	        Car = _car;
+			CarRegistrationNumber  = _carRegistrationNumber;
+			CarColor =  _carColor;
+
+}  
+
+
+    }
+}
+
+		
+
+
+
+	
