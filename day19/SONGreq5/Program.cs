45bd11ed2960872562f37e62759c698ac42fc25f
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-05-02 16:52:09 +0530

    Add files via upload

---
+++ day19/SONGreq5/Program.cs
@@ -0,0 +1,22 @@
+﻿using songreq_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SONGreq5
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> songs = Songrepo.GetSongs();
+
+            SortedDictionary<string, int> sd = Song.CalculateTypeCount(songs);
+
+
+            
+        }
+    }
+}
