a8c525d9acceb3b5864e70b0393be89eb288466b
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-05-08 09:37:08 +0530

    Add files via upload

---
+++ day11/CarPoolingSystem_Requirement/CarPoolingSystemreq4/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Req4
+
+{
+
+    class Cars
+
+    {
+
+        public abstract class Car
+
+        {
+
+            public long Id { get; set; }
+
+            public string Name { get; set; }
+
+            // Abstract method for calculating drive cost
+
+            public abstract double CalculateDriveCost(double km);
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day11/CarPoolingSystem_Requirement/CarPoolingSystemreq4/Hatchback.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using static Req4.Cars;
+
+using System.Xml.Linq;
+
+namespace Req4
+
+{
+
+    internal class Hatchback_class
+
+    {
+
+        // Hatchback class
+
+        public class Hatchback : Car
+
+        {
+
+            public bool PowerWindows { get; set; }
+
+            public bool AutomaticGear { get; set; }
+
+            // Constructor
+
+            public Hatchback(long id, string name, bool powerWindows, bool automaticGear)
+
+            {
+
+                Id = id;
+
+                Name = name;
+
+                PowerWindows = powerWindows;
+
+                AutomaticGear = automaticGear;
+
+            }
+
+            // Implementing abstract method from Car class
+
+            public override double CalculateDriveCost(double km)
+
+            {
+
+                double cost = 0;
+
+                if (AutomaticGear)
+
+                    cost = 12;
+
+                else
+
+                    cost = 10;
+
+                return cost * km;
+
+            }
+
+        }
+
+    }
+
+}
+
+
