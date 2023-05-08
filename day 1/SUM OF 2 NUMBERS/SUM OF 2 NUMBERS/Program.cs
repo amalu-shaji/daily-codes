00f8e4d24808c7fb98839e1a91875059157bfcbf
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-05-08 09:46:42 +0530

    Add files via upload

---
+++ day 1/SUM OF 2 NUMBERS/SUM OF 2 NUMBERS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUM_OF_2_NUMBERS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int sum= a+ b;
+            Console.WriteLine(sum);
+            
+
+
+        }
+    }
+}
