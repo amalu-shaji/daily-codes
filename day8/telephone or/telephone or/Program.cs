b562da161a0dc045c5a1544544ee83d8a4764af4
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-05-09 09:40:54 +0530

    Add files via upload

---
+++ day8/telephone or/telephone or/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace telephone
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            ElectronicPhone myPhone = new ElectronicPhone();
+
+            myPhone.Ring();
+
+        }
+
+        public class Telephone
+
+        {
+
+            protected string phonetype;
+
+            public string Phonetype
+
+            {
+
+                get { return phonetype; }
+
+                set
+
+                {
+
+                    if (phonetype != null)
+
+                        phonetype = value;
+
+                }
+
+            }
+
+            public Telephone()
+
+            {
+
+            }
+
+            public Telephone(string Phonetype)
+
+            {
+
+                phonetype = Phonetype;
+
+            }
+
+            public void Ring()
+
+            {
+
+                Console.WriteLine("Ringing the {0}", phonetype);
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+        public class ElectronicPhone : Telephone
+
+        {
+
+            public ElectronicPhone()
+
+            {
+
+                phonetype = "Digital";
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
