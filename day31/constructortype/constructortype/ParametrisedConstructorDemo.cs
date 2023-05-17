aa216bb848223faec0dacea5ee30f0944851eadc
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-05-17 09:26:49 +0530

    Add files via upload

---
+++ day31/constructortype/constructortype/ParametrisedConstructorDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructortype
+{
+    class ParametrisedConstructorDemo
+    {
+        int x;
+        public ParametrisedConstructorDemo(int i)
+        {
+            Console.WriteLine(" parametrised constructor is called" + i);
+        }
+        public void display()
+        {
+
+            Console.WriteLine("value of x is:" + x);
+        }
+    }
+
+}
---
+++ day31/constructortype/constructortype/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructortype
+{
+    class Program
+    {
+
+
+        //        static void Main(string[] args)
+        //        {
+        //            student obj1 = new student();
+        //            student obj2 = new student();
+        //            student obj3 = new student();
+
+
+        //        }
+        //    }
+        //    class student
+        //    {
+        //        public  student()
+        //        {
+        //            Console.WriteLine( "print reszult");
+        //        }
+        //    }
+        //}
+        static void Main()
+        {
+            ParametrisedConstructorDemo cd = new ParametrisedConstructorDemo(10);
+            cd.display();
+            Console.ReadLine();
+
+        }
+
+    }
+}
