261c4569cb6e89ed68bc639e629bb66769fcc6e1
amalu-shaji <129927909+amalu-shaji@users.noreply.github.com>
2023-05-25 15:22:46 +0530

    Add files via upload

---
+++ day39/PRIME NUMBER/PRIME NUMBER/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIME_NUMBER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the desired max number to display prime number");
+            int n = int.Parse(Console.ReadLine());
+            bool isPrimenumber = true;
+            Console.WriteLine("the prime number betwwen {0} and {1} is:", 2, n);
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 2; j <= n; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrimenumber = false;
+                        break;
+                    }
+                }
+                if (isPrimenumber)
+                {
+                    Console.WriteLine(i);
+                }
+                isPrimenumber = true;
+            }
+        }
+    }
+}
---
+++ day39/code_challange/code_challange/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace sample
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter the student type:");
+
+            string Studenttype = Console.ReadLine();
+
+            Console.WriteLine("Enter the tution fee:");
+
+            float TutionFee = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the bus fee:");
+
+            float BusFee = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the hostel fee:");
+
+            float HostelFee = float.Parse(Console.ReadLine());
+
+            if (Studenttype == "MSDS")
+
+            {
+
+                double Fee1 = TutionFee + BusFee;
+
+                Console.WriteLine("The fees to be paid by the student is " +string.Format("{0:0.00}", Fee1));
+
+            }
+
+            else if (Studenttype == "MSH")
+
+            {
+
+                double Fee2 = TutionFee + HostelFee;
+
+                Console.WriteLine("The fees to be paid by the student is " + string.Format("{0:0.00}", Fee2));
+
+            }
+
+            else if (Studenttype == "MGSDS")
+
+            {
+
+                double Fee3 = 1.5 * TutionFee + BusFee;
+
+                Console.WriteLine("The fees to be paid by the student is " + string.Format("{0:0.00}", Fee3));
+
+            }
+
+            else if (Studenttype == "MGSH")
+
+            {
+
+                double Fee4 = 1.5 * TutionFee + HostelFee;
+
+                Console.WriteLine("The fees to be paid by the student is " + string.Format("{0:0.00}", Fee4));
+
+            }
+
+        }
+
+    }
+
+}
